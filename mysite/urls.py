04c5c456eb01d77fbc17a711c2de0b66fda55848
Scott Edwards <scott.edwards3@gmail.com>
2016-07-06 08:42:11 -0400

    initial commit

---
+++ mysite/urls.py
@@ -0,0 +1,53 @@
+from __future__ import unicode_literals
+
+from django.conf.urls import include, url
+from django.conf.urls.i18n import i18n_patterns
+from django.contrib import admin
+from django.views.i18n import set_language
+from django.views.decorators.csrf import ensure_csrf_cookie
+
+from myblog.views import contact_me
+
+from mezzanine.core.views import direct_to_template
+from mezzanine.conf import settings
+from mezzanine.blog.views import blog_post_list
+
+admin.autodiscover()
+
+# Add the urlpatterns for any custom Django applications here.
+# You can also change the ``home`` view to add your own functionality
+# to the project's homepage.
+
+urlpatterns = i18n_patterns(
+    # Change the admin prefix here to use an alternate URL for the
+    # admin interface, which would be marginally more secure.
+    url("^admin/", include(admin.site.urls)),
+)
+
+if settings.USE_MODELTRANSLATION:
+    urlpatterns += [
+        url('^i18n/$', set_language, name='set_language'),
+    ]
+
+urlpatterns += [
+    url("^$", ensure_csrf_cookie(blog_post_list), {'template': 'myblog/blog_post_list.html'}, name="home"),
+    url("^contact_me/$", contact_me, name="contact_me"),
+
+    # MEZZANINE'S URLS
+    # ----------------
+    # ADD YOUR OWN URLPATTERNS *ABOVE* THE LINE BELOW.
+    # ``mezzanine.urls`` INCLUDES A *CATCH ALL* PATTERN
+    # FOR PAGES, SO URLPATTERNS ADDED BELOW ``mezzanine.urls``
+    # WILL NEVER BE MATCHED!
+
+    # If you'd like more granular control over the patterns in
+    # ``mezzanine.urls``, go right ahead and take the parts you want
+    # from it, and use them directly below instead of using
+    # ``mezzanine.urls``.
+    url("^", include("mezzanine.urls")),
+]
+
+# Adds ``STATIC_URL`` to the context of error pages, so that error
+# pages can use JS, CSS and images.
+handler404 = "mezzanine.core.views.page_not_found"
+handler500 = "mezzanine.core.views.server_error"
